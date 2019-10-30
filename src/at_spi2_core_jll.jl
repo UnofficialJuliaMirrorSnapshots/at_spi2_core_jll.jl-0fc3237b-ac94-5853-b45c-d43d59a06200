347c2a3edaf071d683311403ab2848707d70626e
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 18:17:21 +0000

    Snapshot of branch master taken on 2019-10-30T14:17:20.626-04:00 from "https://github.com/UnofficialJuliaMirror/at_spi2_core_jll.jl-0fc3237b-ac94-5853-b45c-d43d59a06200"

---
+++ src/at_spi2_core_jll.jl
@@ -0,0 +1,31 @@
+module at_spi2_core_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("0fc3237b-ac94-5853-b45c-d43d59a06200"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "at_spi2_core", artifacts_toml) for e in artifacts["at_spi2_core"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load at_spi2_core; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module at_spi2_core_jll
